18030484c3444de70a67d3e42c151bd070f1968a
SowmikaRepala <168825063+SowmikaRepala@users.noreply.github.com>
2025-09-02 02:42:20 +0530

    Updated InterviewItemCard and InterviewList to show job details and formatted date

--- app/dashboard/_components/InterviewItemCard.jsx
+++ app/dashboard/_components/InterviewItemCard.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
-function InterviewItemCard() {
+function InterviewItemCard({interview}) {
+
   return (
-    <div>InterviewItemCard</div>
+    <div className='border shadow-sm rounded-lg p-3'>
+    <h2 className='font-bold text-primary'>{interview.jobPosition}</h2>
+      <h2  className='text-sm text-gray-600'>{interview?.jobExperience} Years of Experience</h2>
+      <h2 className='text-xs text-gray-400'>
+  Created At: {new Date(interview.createdAt).toLocaleDateString()}
+</h2>
+      </div>
   )
 }
 
--- app/dashboard/_components/InterviewList.jsx
+++ app/dashboard/_components/InterviewList.jsx
@@ -15,22 +15,33 @@ function InterviewList() {
        user && GetInterviewList();
     },[user])
     const GetInterviewList= async()=>{
+        const email = user?.primaryEmailAddress?.emailAddress
         const result = await db.select()
         .from(MockInterview)
-        .where(eq(MockInterview.createdBy,user?.primaryEmailAddress))
+        .where(eq(MockInterview.createdBy,email))
         .orderBy(desc(MockInterview.id));
-
+        
+       
         console.log(result);
         setInterviewList(result);
     }
+    console.log("InterviewList:", interviewList);
   return (
     <div>
         <h2 className='font-medium text-xl'>Previous Mock InterviewList</h2>
-         <div>
-            {interviewList&& interviewList.map((interview,index)=>(
-                <InterviewItemCard key={index}/>
-            ))}
-         </div>
+       
+         <div className='grid grid-cols-1 md:grid-cols lg:grid-cols-2 gap-5 my-3'>
+          {interviewList && interviewList.map((interview)=>(
+           <InterviewItemCard 
+            key={interview.id}
+           interview={interview}
+          /> 
+
+         ))}
+         
+         
+        
+   </div>
     </div>
   )
 }
